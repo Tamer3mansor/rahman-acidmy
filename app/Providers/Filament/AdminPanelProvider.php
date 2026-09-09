1ddc48d6ae8ce911073eb4f8dafa91ec7a1fd7cf
tamer3mansor <tamermansor371@gmail.com>
2026-09-09 23:22:36 +0300

    feat: Update dark mode configuration in admin panel for improved flexibility

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,7 +52,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login(CustomLogin::class)
             ->profile()
-            ->darkMode(isForced: true)
+            ->darkMode()
             ->colors([
                 'primary' => Color::hex('#2D6A4F'),
             ])
