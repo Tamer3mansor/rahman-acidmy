f26484ebf57be2357bb5efef71fbcb9a14704172
tamer3mansor <tamermansor371@gmail.com>
2026-09-09 19:57:52 +0300

    feat: Add getMaxWidth method to CustomLogin for full width layout

--- app/Filament/Pages/Auth/CustomLogin.php
+++ app/Filament/Pages/Auth/CustomLogin.php
@@ -4,10 +4,16 @@ namespace App\Filament\Pages\Auth;
 
 use App\Models\SystemSettings;
 use Filament\Auth\Pages\Login;
+use Filament\Support\Enums\Width;
 
 class CustomLogin extends Login
 {
     protected string $view = 'filament.pages.auth.login';
+
+    protected function getMaxWidth(): Width | string | null
+    {
+        return Width::Full;
+    }
 
     public function getLogo(): ?string
     {
