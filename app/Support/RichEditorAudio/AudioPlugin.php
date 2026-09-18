650ceaaf38746873824daf965c6d43bf73e7d613
tamer3mansor <tamermansor371@gmail.com>
2026-09-18 12:25:48 +0300

    fix: update audio extension JavaScript file path to include version query

--- app/Support/RichEditorAudio/AudioPlugin.php
+++ app/Support/RichEditorAudio/AudioPlugin.php
@@ -56,7 +56,7 @@ class AudioPlugin implements HasToolbarButtons, RichContentPlugin
     public function getTipTapJsExtensions(): array
     {
         return [
-            '/js/rich-editor-audio-extension.js',
+            '/js/rich-editor-audio-extension.js?v=2',
         ];
     }
 
